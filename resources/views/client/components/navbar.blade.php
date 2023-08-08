5b58365c3a207abc4c794934ec2b3ffb0485a32a
marzanafroz <marzanafroz97@gmail.com>
2023-08-08 18:55:33 +0600

    logo

--- resources/views/client/components/navbar.blade.php
+++ resources/views/client/components/navbar.blade.php
@@ -38,8 +38,8 @@
 <div class="container-fluid sticky-top bg-white shadow-sm">
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
-            <a href="index.html" class="navbar-brand">
-                <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>LifeCare</h1>
+            <a class="py-2"  href="{{url("/")}}">
+                <img class="logo" src="{{asset("assets/client/img/aa.png")}}" alt="" srcset="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
