21389cec5c0b22373cd72a29ede916267523eecc
SR-Sohan <sr.sohanur22@gmail.com>
2023-08-08 18:17:39 +0600

    doctor find

--- resources/views/client/components/navbar.blade.php
+++ resources/views/client/components/navbar.blade.php
@@ -39,7 +39,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
             <a href="index.html" class="navbar-brand">
-                <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>Medinova</h1>
+                <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>LifeCare</h1>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
--- resources/views/client/pages/doctor.blade.php
+++ resources/views/client/pages/doctor.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
+<div class="find_doctor my-5">
 <div class="container">
-
  <form action="" method="post" class="form-control">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-4 offset-md-1">
             <div class="">
 
                 <label for="selectOption">Select Branch:</label>
@@ -30,23 +30,14 @@
             </select>
             </div>
         </div>
-
-        
-         <div class="col-md-4">
+         <div class="col-md-2">
             <div class="">
-                <br>
-                <button class="form-control">Find Doctor</button>
+                <br> 
+                <button class="btn btn-primary">Find Doctor</button>
             </div>
          </div>
-       
-    
-        
     </div>
-    
-    
-
- </form>
-
-</div>
-    
+  </form>
+  </div>
+</div> 
 @endsection
